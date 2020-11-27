e1ba81980a8a45d611917388afae5a092ec430f0
gronkybong <48972370+gronkybong@users.noreply.github.com>
2020-11-26 21:03:33 -0800

    made temp auto go to park after shooting

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TempAuto.java
@@ -20,29 +20,14 @@ public class TempAuto extends LinearOpMode {
         telemetry.update();
         robot.driveStraightByDistance(robot.DIRECTION_RIGHT, 300, 0.3);
         robot.toggleShooter(true);
-        if (numberOfRings == 0) {
-            //drive to shooting position
-            robot.driveStraightByGyro(robot.DIRECTION_FORWARD, 1200, 0.7, false);
-            robot.driveStraightByDistance(robot.DIRECTION_LEFT, 850, 0.6);
-            for (int i = 0; i < 3; i++) {
-                robot.launchRing(true);
 
-            }
-        } else if (numberOfRings == 1) {
-            //drive to shooting position
-            robot.driveStraightByGyro(robot.DIRECTION_FORWARD, 1200, 0.7, false);
-            robot.driveStraightByDistance(robot.DIRECTION_LEFT, 500, 0.6);
-            for (int i = 0; i < 3; i++) {
-                robot.launchRing(true);
-            }
-        } else if (numberOfRings == 4) {
-            //drive to shooting position
-            robot.driveStraightByGyro(robot.DIRECTION_FORWARD, 1200, 0.7, false);
-            robot.driveStraightByDistance(robot.DIRECTION_LEFT, 500, 0.6);
-            for (int i = 0; i < 3; i++) {
-                robot.launchRing(true);
-            }
+        //drive to shooting position
+        robot.driveStraightByGyro(robot.DIRECTION_FORWARD, 1200, 0.7, false);
+        robot.driveStraightByDistance(robot.DIRECTION_LEFT, 850, 0.6);
+        for (int i = 0; i < 3; i++) {
+            robot.launchRing(true);
         }
+        robot.driveStraightByDistance(robot.DIRECTION_FORWARD, 400, 0.6);
 
     }
 
