26b493c270c5e06689afcedc1ce4ac91da05b726
gronkybong <48972370+gronkybong@users.noreply.github.com>
2021-01-11 14:17:56 -0800

    Update WobbleGoalBot.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/WobbleGoalBot.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/bot/WobbleGoalBot.java
@@ -6,7 +6,8 @@ import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.hardware.Servo;
 
 public class WobbleGoalBot extends ShooterBot {
-    public Servo wobbleArm = null;
+    public Servo wobblePinch = null;
+    public DcMotor wobbleArm = null;
 
     //two positions of the wobble servo
     final double wobbleRetracted = 0.25;
@@ -21,27 +22,27 @@ public class WobbleGoalBot extends ShooterBot {
     @Override
     public void init(HardwareMap ahwMap) {
         super.init(ahwMap);
-        wobbleArm = hwMap.get(Servo.class, "WobbleGoal");
-        wobbleArm.setPosition(wobbleRetracted);
+        wobblePinch = hwMap.get(Servo.class, "WobbleGoal");
+        wobblePinch.setPosition(wobbleRetracted);
     }
 
     //call openArm() to open the arm
     public void openArm() {
-        wobbleArm.setPosition(wobbleExtended);
+        wobblePinch.setPosition(wobbleExtended);
     }
 
     //call closeArm() to close the arm
     public void closeArm() {
-        wobbleArm.setPosition(wobbleRetracted);
+        wobblePinch.setPosition(wobbleRetracted);
     }
 
     public void toggleWobble(boolean button) {
         if (button) {
             if (isOpen) {
-                wobbleArm.setPosition(wobbleRetracted);
+                wobblePinch.setPosition(wobbleRetracted);
 
             } else if (isOpen = false) {
-                wobbleArm.setPosition(wobbleExtended);
+                wobblePinch.setPosition(wobbleExtended);
 
             }
         }
